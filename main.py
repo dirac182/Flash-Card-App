72965b9c86e2f9d6d6f755244b88d243195d803a
Derrick <dmorin9696@yahoo.com>
2023-02-04 03:36:18 -0600

    Added word generator

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
 from tkinter import *
+import pandas
+import random
 BACKGROUND_COLOR = "#B1DDC6"
 BLACK = "#2C3333"
 
-#Read CSV File
-import pandas
-import random
+#Read CSV Filez
+
 file = pandas.read_csv("data/spanish_words.csv")
 data = pandas.DataFrame(file)
+data_dict = data.to_dict()
 sp_words = data["spanish"].tolist()
-random_word = random.choice(sp_words)
+def word_gen():
+    random_word = random.choice(sp_words)
+    translation = data_dict[random_word]
+    print(translation)
+    word.config(text=random_word)
 
 
 
@@ -31,11 +37,11 @@ word.grid()
 flashcard.create_window(400,263,window=word)
 
 right_img = PhotoImage(file="images/right.png")
-right_button = Button(image=right_img)
+right_button = Button(image=right_img,command=word_gen)
 right_button.grid(column=1,row=1,)
 
 wrong_img = PhotoImage(file="images/wrong.png")
-wrong_button = Button(image=wrong_img)
+wrong_button = Button(image=wrong_img,command=word_gen)
 wrong_button.grid(column=0,row=1)
 
 
