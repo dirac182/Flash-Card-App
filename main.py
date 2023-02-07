4b5f5d7e759755cc9073fc25bef6a25db82a3497
dirac182 <dmorin9696@yahoo.com>
2023-02-06 21:09:14 -0600

    Almost done.

--- main.py
+++ main.py
@@ -8,24 +8,49 @@ BLACK = "#2C3333"
 
 file = pandas.read_csv("data/spanish_words.csv")
 data = pandas.DataFrame(file)
-data_dict = data.to_dict()
+data_dict = dict(zip(data.spanish,data.english))
 sp_words = data["spanish"].tolist()
+
+try:
+    with open(file="words_to_learn.csv",mode="r") as file:
+        data = pandas.read_csv()
+
+except FileNotFoundError:
+    data.to_csv("words_to_learn.csv",index=False)
+
+
+
+random_word = ""
+translation = ""
+
+
 def word_gen():
+    global random_word
+    global translation
     random_word = random.choice(sp_words)
     translation = data_dict[random_word]
-    print(translation)
+    flashcard.create_image(400, 263, image=card_front)
+    flashcard.itemconfig(front_img,image=card_front)
+    lang.config(text="Spanish")
     word.config(text=random_word)
+    window.after(3000, flip_card)
 
-
+def flip_card():
+    flashcard.create_image(400, 263, image=card_back)
+    flashcard.itemconfig(back_img, image=card_back)
+    lang.config(text="English")
+    word.config(text=translation)
 
 window = Tk()
 window.title("Flash Cards")
-#window.minsize(width=1000,height=750)
 window.config(padx=50,pady=50,bg=BACKGROUND_COLOR)
 
 flashcard = Canvas(height=526,width=800,bg=BACKGROUND_COLOR,highlightthickness=0)
-card_bg = PhotoImage(file="images/card_front.png")
-flashcard.create_image(400,263,image=card_bg)
+card_back = PhotoImage(file="images/card_back.png")
+card_front = PhotoImage(file="images/card_front.png")
+back_img = flashcard.create_image(400, 263, image=card_back)
+front_img = flashcard.create_image(400,263,image=card_front)
+
 flashcard.grid(column=0,row=0,columnspan=2)
 #Language Label
 lang = Label(flashcard,text="Spanish",font=("Ariel",40,"italic"),fg=BLACK)
